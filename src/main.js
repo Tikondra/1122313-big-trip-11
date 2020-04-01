3d2901a8c291f5719f3d69a5a2813f654e628bee
Tikondra <tikondra47@gmail.com>
2020-04-01 10:41:09 +0400

    Добавила дни и события

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const EVENT_COUNT = 3;
+
 const Place = {
   AFTERBEGIN: `afterbegin`,
   BEFOREEND: `beforeend`
@@ -280,6 +282,66 @@ const createEventEdit = () => {
   );
 };
 
+const creatTripDaysCont = () => `<ul class="trip-days"></ul>`;
+
+const createEventDay = () => {
+  return (
+    `<li class="trip-days__item  day">
+        <div class="day__info">
+          <span class="day__counter">1</span>
+          <time class="day__date" datetime="2019-03-18">MAR 18</time>
+        </div>
+        <ul class="trip-events__list">
+        </ul>
+      </li>`
+  );
+};
+
+const createEvent = () => {
+  return (
+    `<li class="trip-events__item">
+      <div class="event">
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">Taxi to Amsterdam</h3>
+
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            &mdash;
+            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          </p>
+          <p class="event__duration">30M</p>
+        </div>
+
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">20</span>
+        </p>
+
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          <li class="event__offer">
+            <span class="event__offer-title">Order Uber</span>
+            &plus;
+            &euro;&nbsp;<span class="event__offer-price">20</span>
+           </li>
+        </ul>
+
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div>
+    </li>`
+  );
+};
+
+const renderCard = (container, elem, count) => {
+  for (let i = 0; i < count; i++) {
+    render(container, elem, Place.BEFOREEND);
+  }
+};
+
 const render = (container, template, place) => container.insertAdjacentHTML(place, template);
 
 const init = () => {
@@ -296,6 +358,16 @@ const init = () => {
   render(tripEvents, createSort(), Place.AFTERBEGIN);
 
   render(tripEvents, createEventEdit(), Place.BEFOREEND);
+
+  render(tripEvents, creatTripDaysCont(), Place.BEFOREEND);
+
+  const tripDays = tripEvents.querySelector(`.trip-days`);
+
+  render(tripDays, createEventDay(), Place.BEFOREEND);
+
+  const eventList = tripDays.querySelector(`.trip-events__list`);
+
+  renderCard(eventList, createEvent(), EVENT_COUNT);
 };
 
 init();
