322e9264b6fed4abb6fe045a9b765f01197dad95
Tikondra <tikondra47@gmail.com>
2020-04-19 20:57:12 +0400

    Добавляет класс Day

--- src/components/event-day.js
+++ src/components/event-day.js
@@ -1,15 +1,37 @@
-import {generateEvent, generateEvents} from "../Mocks/event-mock";
-import {EvenOption} from "./consts";
-import {createEvents} from "./events";
-import {createDayInfo} from "./day-info";
+import {createElement} from "./utils";
 
-export const createEventDay = () => {
-  const events = generateEvents(EvenOption.COUNT, generateEvent);
+const createEventDay = (day) => {
+  const {dayCounter, dateTime, dayNumber, month} = day;
 
   return (
-    `<li class="trip-days__item  day">
-        ${createDayInfo()}
-        ${createEvents(events)}
+    `<li class="trip-days__item  day" id="day${dayCounter}">
+        <div class="day__info">
+          <span class="day__counter">${dayCounter}</span>
+          <time class="day__date" datetime="${dateTime}">${month} ${dayNumber}</time>
+        </div>
      </li>`
   );
 };
+
+export default class Day {
+  constructor(day) {
+    this._day = day;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createEventDay(this._day);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
