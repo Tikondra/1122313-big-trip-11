8dc69e509a150f4e4d1e11e38672e4cb08e3dfd0
Tikondra <tikondra47@gmail.com>
2020-05-21 15:10:51 +0400

    sw

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
-const STATUS_BASIC = `basic`;
-const STATUS_SUCCESS = 200;
 const CACHE_PREFIX = `bigtrip-cache`;
 const CACHE_VER = `v1`;
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VER}`;
+const STATUS_BASIC = `basic`;
+const STATUS_SUCCESS = 200;
 
 const DATA = [
   `/`,
@@ -24,56 +24,65 @@ const DATA = [
   `/img/logo.png`
 ];
 
-const getData = (cache) => cache.addAll(DATA);
-
 const onInstall = (evt) => {
-  evt.waitUntil(caches.open(CACHE_NAME).then(getData));
-};
-
-const getKey = (key) => {
-  if (key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME) {
-    return caches.delete(key);
-  }
-
-  return null;
-};
-
-const getFilterKey = (key) => key !== null;
-
-const getAllKeys = (keys) => {
-  Promise.all(keys.map(getKey).filter(getFilterKey));
-};
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(DATA);
+      })
+  );
+}
 
 const onActivate = (evt) => {
-  evt.waitUntil(caches.keys().then((keys) => getAllKeys(keys)));
-};
+  evt.waitUntil(
 
-const getRequest = (response, request) => {
-  if (!response || response.status !== STATUS_SUCCESS || response.type !== STATUS_BASIC) {
-    return response;
-  }
+    caches.keys()
+      .then(
 
-  const clonedResponse = response.clone();
+        (keys) => Promise.all(
+          keys.map(
+            (key) => {
 
-  caches.open(CACHE_NAME)
-    .then((cache) => cache.put(request, clonedResponse));
+              if (key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME) {
+                return caches.delete(key);
+              }
 
-  return response;
-};
-
-const getCacheResponse = (cacheResponse, request) => {
-  if (cacheResponse) {
-    return cacheResponse;
-  }
-
-  return fetch(request).then(getRequest);
-};
+              return null;
+            })
+            .filter((key) => key !== null)
+        )
+      )
+  );
+}
 
 const onFetch = (evt) => {
   const {request} = evt;
 
-  evt.respondWith(caches.match(request).then((cacheResponse) => getCacheResponse(cacheResponse, request)));
-};
+  evt.respondWith(
+    caches.match(request)
+      .then((cacheResponse) => {
+
+        if (cacheResponse) {
+          return cacheResponse;
+        }
+
+        return fetch(request)
+          .then((response) => {
+
+            if (!response || response.status !== STATUS_SUCCESS || response.type !== STATUS_BASIC) {
+              return response;
+            }
+
+            const clonedResponse = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => cache.put(request, clonedResponse));
+
+            return response;
+          });
+      })
+  );
+}
 
 self.addEventListener(`install`, onInstall);
 self.addEventListener(`activate`, onActivate);
