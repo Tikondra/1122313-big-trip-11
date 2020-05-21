c8295194a521ac2a239b2f5190fb8cbcc0b3dc14
Tikondra <tikondra47@gmail.com>
2020-05-21 22:19:10 +0400

    добавляет отражение даты в хедере

--- src/components/header-info.js
+++ src/components/header-info.js
@@ -1,26 +1,12 @@
 import {createTripInfo} from "./trip-info";
 import {createTripCost} from "./trip-cost";
 import AbstractComponent from "./abstract-component";
-import {getSortByDate} from "../utils/common";
-
-const getCities = (points) => {
-  const sortedPoints = getSortByDate(points);
-
-  return sortedPoints.reduce((cityList, point) => {
-    if (!cityList.includes(point.destinations.name)) {
-      cityList.push(point.destinations.name);
-    }
-
-    return cityList;
-  }, []);
-};
 
 const createHeaderInfo = (points) => {
-  const cities = getCities(points);
 
   return (
     `<section class="trip-main__trip-info  trip-info">
-            ${createTripInfo(cities)}
+            ${createTripInfo(points)}
             ${createTripCost()}
      </section>`
   );
--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -1,3 +1,7 @@
+import {getSortByDate} from "../utils/common";
+import {Format} from "./consts";
+import moment from "moment";
+
 const getTitle = (cities) => {
   const countCity = cities.length;
 
@@ -10,13 +14,53 @@ const getTitle = (cities) => {
   return cities;
 };
 
-export const createTripInfo = (cities) => {
+const getCities = (points) => {
+
+  return points.reduce((cityList, point) => {
+    if (!cityList.includes(point.destinations.name)) {
+      cityList.push(point.destinations.name);
+    }
+
+    return cityList;
+  }, []);
+};
+
+const getDate = (dates) => {
+  const from = dates[0];
+
+  if (dates.length >= 2) {
+    const to = dates[dates.length - 1];
+
+    return `${from} &mdash; ${to}`;
+  } else if (dates.length === 1) {
+    return `${from}`;
+  }
+
+  return ``;
+};
+
+const getDates = (points) => {
+
+  return points.reduce((dateList, point) => {
+    const date = moment(point.timeStart).format(Format.DAY_DATE);
+    if (!dateList.includes(date)) {
+      dateList.push(date);
+    }
+
+    return dateList;
+  }, []);
+};
+
+export const createTripInfo = (points) => {
+  const sortedPoints = getSortByDate(points);
+  const cities = getCities(sortedPoints);
+  const dates = getDates(sortedPoints);
 
   return (
     `<div class="trip-info__main">
       <h1 class="trip-info__title">${getTitle(cities)}</h1>
 
-      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+      <p class="trip-info__dates">${getDate(dates)}</p>
     </div>`
   );
 };
